2c50f87459a6c77fdfb7302f8da02906fee01c25
Sergio Mendoza <sm8a76@gmail.com>
2013-06-07 19:05:15 -0500

    Updated until Part 9: View Routing (Step 3)
    With failures so far.

---
+++ js/HomeView.js
@@ -0,0 +1,41 @@
+var HomeView = function(store) {
+	
+    this.render = function() {
+        /*var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"*/
+        this.el.html(HomeView.template());
+        return this;
+    },  	
+    
+    this.findByName = function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
+            /*if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            } */           
+        });
+    };	
+    
+ 
+    this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
+ 
+    this.initialize();
+    
+    HomeView.template = Handlebars.compile($("#home-tpl").html());
+    HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
+ 
+}
+
+
--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ var app = {
     },*/
 		
 		
-	findByName: function() {
+	/*findByName: function() {
 		    var self = this;
 		    this.store.findByName($('.search-key').val(), function(employees) {
 		        $('.employee-list').html(self.employeeLiTpl(employees));
 		    });
-	},		
+	},*/		
 		
     
     showAlert: function (message, title) {
@@ -30,29 +30,75 @@ var app = {
         }
     },    
     
-    renderHomeView: function() {
-        /*var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"*/
+    /*renderHomeView: function() {
         $('body').html(this.homeTpl);
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },    
+    },*/  	  
 
-    initialize: function() {
+    /*initialize: function() {
     	var self = this;
     	this.homeTpl = Handlebars.compile($("#home-tpl").html());
     	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());    	
     	    	
         this.store = new MemoryStore(function() {
-            //self.showAlert('Store Initialized', 'Info');
         	self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    }*/
+    
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+        
+        $(window).on('hashchange', $.proxy(this.route, this));
+    },  
+    
+    /*route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },*/
+    
+    initialize: function() {
+        var self = this;
+        this.detailsURL = '/^#employees\/(\d{1,})/';
+        this.registerEvents(); 
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        	//self.route();
+        });
+        
+    }	    
 
+    
 };
 
 app.initialize();
+
+
+
